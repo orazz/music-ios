019b2fcc793de7b5a9039147637bc33304703cab
Orazz Atakishiyev <orazz.tm@gmail.com>
2015-05-09 18:36:00 +0500

    Initial Commit

---
+++ vkMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vkMusic
+//
+//  Created by Atakishiyev Orazdurdy on 5/9/15.
+//  Copyright (c) 2015 veriloft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
